65fd699a667bc4e35cb3615304e890ca1aad1f6a
Hitesh-Kriplani <hiteshkriplani00@gmail.com>
2022-05-24 10:10:56 +0530

    Repo Created

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyC5w6v75z7NVLF5Fxp9aytidPKKVNqg-ug',
-  authDomain: 'todo-app-react-firebase-b57c3.firebaseapp.com',
-  projectId: 'todo-app-react-firebase-b57c3',
-  storageBucket: 'todo-app-react-firebase-b57c3.appspot.com',
-  messagingSenderId: '33960739380',
-  appId: '1:33960739380:web:954d7f10e04f8f79e4c061',
-  measurementId: 'G-75BGGWY1E3',
-};
+import { firebaseConfig } from './.config';
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
